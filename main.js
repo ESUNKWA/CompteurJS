8fd9856c237c1417371e42e77e4ca6ff9e42998e
DKEM <kouadiodeki@gmail.com>
2021-04-29 18:28:18 +0000

    compteur dynamique en js terminé

--- main.js
+++ main.js
@@ -1,61 +1,71 @@
  $(function(){
     let obj = {};
     let dte;
-    let dateLancmnt;
-    let jj, hh, mm, ss;
+    let dtesys = new Date();
+    var dateLancmnt;
 
       let btn = $('button');
       let dateslect = $('#dateselct');
       $(btn).on('click', (event)=>{
+
           event.preventDefault();
+
           dte = $(dateslect).val();
-         dates = dte.split('T')[0];
-         heures = dte.split('T')[1];
+          
+         let datesChaine = dte.split('T')[0];
+         let heuresChaine = dte.split('T')[1];
 
+         if( dte == ''){
+             alert('Veuillez sélectionner la date et l\'heure');
+             return
+         }
 
+         let datesChaineTab = datesChaine.split('-');
+         let heuresChaineTab = heuresChaine.split(':');
 
-         obj.year = dates.split('-')[0];
-         obj.month = dates.split('-')[1];
-         obj.dd = dates.split('-')[2];
+         obj.year = datesChaineTab[0];
+         obj.month = datesChaineTab[1]-1;
+         obj.dd = datesChaineTab[2];
 
-         obj.hh = heures.split(':')[0];
-         obj.mm = heures.split(':')[1];
-         obj.ss = heures.split(':')[2];
-         let dateLancmnt = new Date(obj.year,3,30,15,30,0);
-         console.log(obj )
-         console.log(dateLancmnt )
-
+         obj.hh = heuresChaineTab[0];
+         obj.mm = heuresChaineTab[1];
+         obj.ss = 0;
+         
+         dateLancmnt = new Date(obj.year,obj.month,obj.dd,obj.hh,obj.mm,obj.ss);
+         dateLancmnt1 = new Date(2021,03,01,17,45,0);
+         console.log(obj);
+       
          fctCompteur();
 
+  
       });
 
-
-function fctCompteur(){
+      function fctCompteur(){
+        
+        let jj, hh, mm;
+        let dateAct = new Date();
+        let ss = ( dateLancmnt.getTime() - dateAct.getTime() ) / 1000;//durée en seconde
+        
+        jj = Math.floor(ss / 86400);
+        
+        ss = ss - (jj * 86400);
     
-    let dateAct = new Date();
-    let ss = ( dateLancmnt.getTime() - dateAct.getTime() ) / 1000;//durée en seconde
+        hh = Math.floor(ss / 3600);
     
-    jj = Math.floor(ss / 86400);
+        ss = ss - (hh * 3600);
+         
+        mm         = Math.floor( ss / 60 );
     
-    ss = ss - (jj * 86400);
-
-    hh = Math.floor(ss / 3600);
-
-    ss = ss - (hh * 3600);
-     
-    mm         = Math.floor( ss / 60 );
-
-    ss         = Math.floor(ss - (mm * 60));
-
-     document.getElementById('jj').innerHTML = (jj < 10) ? '0'+jj : jj;
-     document.getElementById('hh').innerHTML = (hh < 10) ? '0'+hh : hh;
-     document.getElementById('mm').innerHTML = (mm < 10) ? '0'+mm : mm;
-     document.getElementById('ss').innerHTML = (ss < 10) ? '0'+ss : ss;
-
-    setTimeout(fctCompteur, 1000)
-
-
-  }
+        ss         = Math.floor(ss - (mm * 60));
+    
+         document.getElementById('jj').innerHTML = (jj < 10) ? '0'+jj : jj;
+         document.getElementById('hh').innerHTML = (hh < 10) ? '0'+hh : hh;
+         document.getElementById('mm').innerHTML = (mm < 10) ? '0'+mm : mm;
+         document.getElementById('ss').innerHTML = (ss < 10) ? '0'+ss : ss;
+    
+        setTimeout(fctCompteur, 1000)
+    
+      }
 
   })
 
